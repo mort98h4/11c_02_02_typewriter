4269f661c749584c876225c47554d9edc7d4aa49
Morten Gross <mg@vuffeli.dk>
2021-02-09 13:56:04 +0100

    Now has sounds

--- typewriter.js
+++ typewriter.js
@@ -16,13 +16,22 @@ function start() {
 
 function nextLetter() {
     console.log("nextLetter");
+
+
+    if (text.charAt(n-1) === " ") {
+        document.querySelector("#typespace").play();
+    } else {
+        let randomSound = Math.floor(Math.random() * 2 + 1);
+        document.querySelector("#typekey"+randomSound).play();
+    }
     
     document.querySelector(".typewritten").textContent = text.substring(0,n);
     n++;
     
     if (n <= text.length) {
-        let randomTimer = Math.floor(Math.random() * (6 - 3) + 3);
-        console.log(randomTimer);
+        let randomTimer = Math.floor(Math.random() * (7 - 4) + 4);
         setTimeout(nextLetter, randomTimer * 100);
+    } else {
+        document.querySelector("#typelast").play();
     }
 }
