24899c40726a675b75cbf0f0992665445eedc9d6
Morten Gross <mg@vuffeli.dk>
2021-02-09 13:06:19 +0100

    minimum requirements are met

--- typewriter.js
+++ typewriter.js
@@ -3,16 +3,23 @@
 window.addEventListener("DOMContentLoaded", start);
 
 const text = document.querySelector(".typewritten").textContent;
+let n = 0;
 
 function start() {
     console.log("start");
     console.log(text);
 
-    document.querySelector("h1").textContent = "";
+    document.querySelector(".typewritten").textContent = "";
 
     nextLetter();
 }
 
 function nextLetter() {
     console.log("nextLetter");
+    
+    document.querySelector(".typewritten").textContent = text.substring(0,n);
+    n++;
+    if (n <= 44) {
+        setTimeout(nextLetter, 500);
+    }
 }
